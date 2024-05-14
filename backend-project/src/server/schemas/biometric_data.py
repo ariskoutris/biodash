10a42378ac42b9268e7a6b95dff2c69bcddd0e0d
Ahmad Khan <ahkhan@student.ethz.ch>
2024-05-14 09:54:08 +0200

    Integrate model into frontend
    Former-commit-id: 820e0fc417bd7cac128981f1fe7961e7277c29bb

--- backend-project/src/server/schemas/biometric_data.py
+++ backend-project/src/server/schemas/biometric_data.py
@@ -1,8 +1,6 @@
 from marshmallow import Schema, fields
 
-
 class BiometricDataSchema(Schema):
-    weight = fields.Float()
-    muscle_mass_perc = fields.Float()
-    fat_mass_perc = fields.Float()
-    fitness_age = fields.Integer()
+    BiometricName = fields.Str()
+    MeasuredOnWeek = fields.List(fields.Int())
+    Value = fields.List(fields.Float())
