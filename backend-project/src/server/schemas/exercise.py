8a566ff871b6eeacb2d5f76a3d549b4ee8456240
ariskoutris <koutrisaris@gmail.com>
2024-05-31 00:06:02 +0200

    Update schema to incorporate aggregate exercise data
    Former-commit-id: ae35df19cfc08732f8481f2c9d47892ab527dd60

--- backend-project/src/server/schemas/exercise.py
+++ backend-project/src/server/schemas/exercise.py
@@ -8,3 +8,17 @@ class ExerciseSchema(Schema):
     duration = fields.Integer()
     calories = fields.Integer()
     mets_min = fields.Float()
+
+class AggExerciseSchema(Schema):
+    week = fields.List(fields.Integer())
+    total_calories_week = fields.List(fields.Float())
+    total_minutes_week = fields.List(fields.Integer())
+    cardio_calories_week = fields.List(fields.Float())
+    cardio_minutes_week = fields.List(fields.Float())
+    isotonic_calories_week = fields.List(fields.Float())
+    isotonic_minutes_week = fields.List(fields.Float())
+    avg_duration_per_workout = fields.List(fields.Float())
+    avg_calories_per_workout = fields.List(fields.Float())
+    avg_metsmin_workout = fields.List(fields.Float())
+    avg_isotonic_workouts = fields.List(fields.Float())
+    avg_cardio_workouts = fields.List(fields.Float())
